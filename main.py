c90324dce2b4b1dcca821431f85bf3d28eb80432
SergMochalov <Serginho19860207@yandex.ru>
2024-09-30 18:45:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import module_1_6
+first = int(input('Введите первое число'))
+second = int(input('Введите второе число'))
+third = int(input('Введите третье число'))
+
+if first==second and third==second:
+    print(3)
+elif first==second or third==second:
+        print(2)
+else:
+    print(0)
